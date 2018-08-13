8bebd6dc082a5acbb24531fee8efbaab048d3968
Seongho Lee <seongho051@naver.com>
2018-08-13 19:48:02 +0900

    feat: Add blog for includes guide

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var app = express();
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {  
-    res.render('index', { title: 'Hey Hey Hey!', message: 'Yo Yo'})
+    res.render('index')
+});
+
+app.get('/blog', function (req, res) {  
+    res.render('blog')
 });
 
 app.listen(3000, function () {  
-    console.log('Example app listening on port 3000!')
+    console.log('Pug tutorial app listening on port 3000!')
 });
